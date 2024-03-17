0ebf8fa40397089d88e201ff0010cb7fda97b0eb
miga-choi <miga.choi.1004@gmail.com>
2024-03-18 00:34:26 +0900

    scheduling-tasks: finish

--- scheduling-tasks/src/main/java/com/example/schedulingtasks/SchedulingTasksApplication.java
+++ scheduling-tasks/src/main/java/com/example/schedulingtasks/SchedulingTasksApplication.java
@@ -2,12 +2,12 @@ package com.example.schedulingtasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchedulingTasksApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SchedulingTasksApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingTasksApplication.class, args);
+    }
 }
