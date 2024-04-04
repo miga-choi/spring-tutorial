429e08a9b4860686ff8c6b312c30c252f88a30e6
miga-choi <miga.choi.1004@gmail.com>
2024-04-05 00:37:46 +0900

    validating-form-input: finish

--- gs-validating-form-input/src/main/java/com/example/validatingforminput/ValidatingFormInputApplication.java
+++ gs-validating-form-input/src/main/java/com/example/validatingforminput/ValidatingFormInputApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ValidatingFormInputApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ValidatingFormInputApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ValidatingFormInputApplication.class, args);
+    }
 
 }
