4315e8caeb0d51869bd5e9a7afec591e57a93610
miga-choi <miga.choi.1004@gmail.com>
2024-04-13 13:39:20 +0900

    caching-gemfire: init

---
+++ gs-caching-gemfire/src/main/java/com/example/cachinggemfire/CachingGemfireApplication.java
@@ -0,0 +1,13 @@
+package com.example.cachinggemfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingGemfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingGemfireApplication.class, args);
+	}
+
+}
---
+++ gs-caching-gemfire/src/test/java/com/example/cachinggemfire/CachingGemfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cachinggemfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingGemfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
