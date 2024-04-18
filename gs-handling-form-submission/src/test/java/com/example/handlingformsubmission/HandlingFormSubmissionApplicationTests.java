47075b3b2dd8650fc086c5010ea5b2a9b80de035
miga-choi <miga.choi.1004@gmail.com>
2024-04-19 00:28:47 +0900

    handling-form-submission: finish

--- gs-handling-form-submission/src/test/java/com/example/handlingformsubmission/HandlingFormSubmissionApplicationTests.java
+++ gs-handling-form-submission/src/test/java/com/example/handlingformsubmission/HandlingFormSubmissionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HandlingFormSubmissionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
