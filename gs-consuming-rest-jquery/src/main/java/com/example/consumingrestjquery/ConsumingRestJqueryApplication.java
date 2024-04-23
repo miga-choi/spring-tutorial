6ba6fe5f3333323e9aae4d6da67ac2dc26bea7bf
miga-choi <miga.choi.1004@gmail.com>
2024-04-24 00:34:18 +0900

    consuming-rest-jquery: init

---
+++ gs-consuming-rest-jquery/src/main/java/com/example/consumingrestjquery/ConsumingRestJqueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumingrestjquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestJqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestJqueryApplication.class, args);
+	}
+
+}
