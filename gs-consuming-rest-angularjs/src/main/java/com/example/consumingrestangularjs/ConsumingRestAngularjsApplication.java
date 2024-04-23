e56c39a13256feb513a48dcffc790373a89465a7
miga-choi <miga.choi.1004@gmail.com>
2024-04-23 21:37:34 +0900

    consuming-rest-angularjs: re-init

---
+++ gs-consuming-rest-angularjs/src/main/java/com/example/consumingrestangularjs/ConsumingRestAngularjsApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumingrestangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestAngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestAngularjsApplication.class, args);
+	}
+
+}
---
+++ gs-consuming-rest-angularjs/src/test/java/com/example/consumingrestangularjs/ConsumingRestAngularjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumingrestangularjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestAngularjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
