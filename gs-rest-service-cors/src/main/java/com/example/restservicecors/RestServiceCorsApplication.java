e5668f9ee5e74e75bb5d24743d6635561774bee7
miga-choi <miga.choi.1004@gmail.com>
2024-04-25 00:36:46 +0900

    rest-service-cors: finish

--- gs-rest-service-cors/src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
+++ gs-rest-service-cors/src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
@@ -2,12 +2,27 @@ package com.example.restservicecors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestServiceCorsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestServiceCorsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceCorsApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/greeting-java-config")
+                        .allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
 
 }
