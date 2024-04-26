6dff50413920210e29f39f881183e332abefe80c
miga-choi <miga.choi.1004@gmail.com>
2024-04-27 01:24:15 +0900

    consuming-web-service: finish

--- gs-consuming-web-service/src/main/java/com/example/consumingwebservice/ConsumingWebServiceApplication.java
+++ gs-consuming-web-service/src/main/java/com/example/consumingwebservice/ConsumingWebServiceApplication.java
@@ -1,13 +1,27 @@
 package com.example.consumingwebservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConsumingWebServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumingWebServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingWebServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner lookup(CountryClient countryClient) {
+        return args -> {
+            String country = "Spain";
+
+            if (args.length > 0) {
+                country = args[0];
+            }
+
+        };
+    }
 
 }
